0c75a1ca634ed3fe98b49589ef93f220debf7e3f
BWACpro <bobwithacamera@gmail.com>
2019-01-18 20:59:21 -0800

    made the file

---
+++ bot.py
@@ -0,0 +1,40 @@
+#https://discordapp.com/api/oauth2/authorize?client_id=437439562386505730&permissions=8&scope=bot
+import discord
+import random
+import praw
+
+#reddit stuff
+reddit = praw.Reddit(client_id='a0nJCwdrD7PDCw',
+                     client_secret='Heu3NLsso54pKHfrbDwBPhnzEWE',
+                     username='-_-BWAC-_-',
+                     password='Babaya98',
+                     user_agent='dank_memz',)
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!hello'):
+    
+    if message.content.startswith('!meme'):
+        random_post = reddit.subreddit('dankmemes').random()
+        random_post_url = random_post.url
+        await message.channel.send(random_post_url)
+        
+    if message.content.startswith('!help'):
+        embed = discord.Embed(title="Help:", description="Desc", color=0x00ff00)
+        embed.add_field(name="Field1", value="hi", inline=False)
+        embed.add_field(name="Field2", value="hi2", inline=False)
+        await client.send_message(message.channel, embed=embed)
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+client = MyClient()
+client.run('token')
