7388b769b850186568970bd6ba70f7a746ea6426
bwac <40532058+BWACpro@users.noreply.github.com>
2019-12-03 14:18:14 +1100

    Update connection.py

--- versions/1.2.4/connection.py
+++ versions/1.2.4/connection.py
@@ -18,11 +18,11 @@ class connection(commands.Cog):
     @commands.command()
     async def connect(self, ctx, username=None):
         reddit = praw.Reddit(client_id='HV16ttsYvjsrRw',
-                             client_secret="StcBK-8Ml-VXM83xFFb0teO5ElM",
-                             password='Redtrucke2',
-                             user_agent='reddit',
-                             username='TheRedditBotDiscord')
-
+                     client_secret="gy6XLBwv_AcRcUZm_fN6Ef-n0Hs",
+                     password='Redtrucke2',
+                     user_agent='reddit',
+                     username='TheRedditBotDiscord')
+        
         user_r = reddit.redditor(username)
 
         code = 0
