942660a5bf6a7616b7a3adfac97e298ba3cad5d8
BWACpro <40532058+BWACpro@users.noreply.github.com>
2019-12-06 09:23:19 +1100

    added topgg api

---
+++ versions/1.2.4/topgg.py
@@ -0,0 +1,20 @@
+import dbl
+import discord
+from discord.ext import commands
+
+
+class topgg(commands.Cog):
+    """Handles interactions with the top.gg API"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" \
+                     ".eyJpZCI6IjQzNzQzOTU2MjM4NjUwNTczMCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTc1NTc2MTU1fQ" \
+                     ".LbuHaiap7xTKvytdqGnkSpgISUp8cbOzyJ4BETm2eYg "
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True)
+
+    async def on_guild_post(self):
+        print("Server count posted successfully")
+
+def setup(bot):
+    bot.add_cog(topgg(bot))
