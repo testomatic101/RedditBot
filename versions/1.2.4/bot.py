0e67ae93522b65058438eb08b6ee976b7a1bc46f
BWACpro <40532058+BWACpro@users.noreply.github.com>
2019-12-09 16:39:31 +1100

    command.command() to commands.command()

--- versions/1.2.4/bot.py
+++ versions/1.2.4/bot.py
@@ -77,7 +77,7 @@ async def feedback(ctx, text=None):
 bot.add_command(feedback)
 
 # update status
-@command.command()
+@commands.command()
 async def update(ctx):
     await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(name="rhelp | In " + str(len(bot.guilds)) + " servers"))
 bot.add_command(update)
