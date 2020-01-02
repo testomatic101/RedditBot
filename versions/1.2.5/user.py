45d9cd2b14f591d8bd5a386699f4899684923cce
bwac <40532058+BWACpro@users.noreply.github.com>
2020-01-02 21:02:17 +1100

    secrets.json

--- versions/1.2.5/user.py
+++ versions/1.2.5/user.py
@@ -12,14 +12,6 @@ secrets = None
 with open('/home/bobwithacamera/secrets.json') as json_file:
     secrets = json.load(json_file)
 
-secrets = None
-if platform == "win32" or "win32":
-    with open('C:/Users/Noahd/Documents/secrets.json') as json_file:
-        secrets = json.load(json_file)
-else:
-    with open('/home/bobwithacamera/secrets.json') as json_file:
-        secrets = json.load(json_file)
-
 
 class user(commands.Cog):
     def __init__(self, bot):
