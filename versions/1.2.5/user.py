1fe7fecc4f3151be1337d2671d51d1cd2a1124c2
bwac <40532058+BWACpro@users.noreply.github.com>
2020-01-02 21:29:12 +1100

    emojis

--- versions/1.2.5/user.py
+++ versions/1.2.5/user.py
@@ -12,6 +12,9 @@ secrets = None
 with open('/home/bobwithacamera/secrets.json') as json_file:
     secrets = json.load(json_file)
 
+trophyemojis = None
+with open('trophyemoji.json') as json_file:
+    trophyemojis = json.load(json_file)
 
 class user(commands.Cog):
     def __init__(self, bot):
