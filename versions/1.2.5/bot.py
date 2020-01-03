3213b8b6f0bde04624bd1536b96baf2ea6f61474
BWACpro <40532058+BWACpro@users.noreply.github.com>
2020-01-03 19:34:06 +1100

    new help message

--- versions/1.2.5/bot.py
+++ versions/1.2.5/bot.py
@@ -8,6 +8,7 @@ from sys import platform
 
 # secrets.json has tokens ect
 secrets = None
+
 if platform == "win32" or "win32":
     with open('C:/Users/Noahd/Documents/secrets.json') as json_file:
         secrets = json.load(json_file)
@@ -44,21 +45,25 @@ async def newhelp(ctx):
     botinfo = await topggclient.get_bot_info()
     getto = botinfo.get('monthlyPoints') + 10
     helpembed.add_field(name="\n\nThe bot currently has **" + str(botinfo.get('monthlyPoints')) + "** votes, can we get it to **" + str(getto) + "**?", value='https://top.gg/bot/437439562386505730/vote', inline=False)
-    helpembed.add_field(name="Found a bug?", value="Report it here http://rbdis.xyz/?page_id=474", inline=False)
+    helpembed.add_field(name="Found a bug?", value="Report it here http://rbdis.xyz/bugreport/", inline=False)
+    helpembed.add_field(name="Please give your feedback!", value="http://rbdis.xyz/feedback", inline=False)
 
-    helpembed.add_field(name="Please give your feedback!", value="http://rbdis.xyz/?page_id=474", inline=False)
-    helpembed.add_field(name="rhelp", value="Shows this page", inline=False)
-    helpembed.add_field(name="rr [sub name here]", value="Gives you some info on a subreddit", inline=False)
-    helpembed.add_field(name="ru [username here]", value="Gives you some info on a user", inline=False)
-    help_user = discord.Embed(title="User commands:",
-                              description="**Commands to add accounts and account management**", color=red)
-    help_user.add_field(name="rconnect [user name here]", value="Connect a reddit connect", inline=False)
-    help_user.add_field(name="runconnect", value="Unconnect the reddit account that you have connected",
-                        inline=False)
-    help_user.add_field(name="rcode", value="Submit the code you got from reddit", inline=False)
-    help_user.add_field(name="rme", value="See your account", inline=False)
-    help_user.set_footer(text="RedditBot " + version)
+    commandsembed = discord.Embed(title="Help:",
+                         description="**Commands**",
+                         color=red)
+    commandsembed.add_field(name="rhelp", value="Shows this page", inline=False)
+    commandsembed.add_field(name="rr [sub name here]", value="Gives you some info on a subreddit", inline=False)
+    commandsembed.add_field(name="ru [username here]", value="Gives you some info on a user", inline=False)
+    # help_user = discord.Embed(title="User commands:",
+    #                           description="**Commands to add accounts and account management**", color=red)
+    # help_user.add_field(name="rconnect [user name here]", value="Connect a reddit connect", inline=False)
+    # help_user.add_field(name="runconnect", value="Unconnect the reddit account that you have connected",
+    #                     inline=False)
+    # help_user.add_field(name="rcode", value="Submit the code you got from reddit", inline=False)
+    # help_user.add_field(name="rme", value="See your account", inline=False)
+    # help_user.set_footer(text="RedditBot " + version)
     await ctx.author.send(embed=helpembed)
+    await ctx.author.send(embed=commandsembed)
 # add the new help command
 bot.add_command(newhelp)
 
