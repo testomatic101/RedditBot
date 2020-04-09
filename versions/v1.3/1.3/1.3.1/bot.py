e40c9202d516a8df5aa1d926b318c1e6a36b9c0c
bwac <40532058+BWACpro@users.noreply.github.com>
2020-04-10 09:30:47 +1000

    deleted utils.py it was dumb lol

--- versions/v1.3/1.3/1.3.1/bot.py
+++ versions/v1.3/1.3/1.3.1/bot.py
@@ -23,8 +23,46 @@ version = '1.3.1 https://rbdis.xyz/ redditbot created by bwac#2517'
 # red for embeds
 red = 0xFF0000
 
+if production:
+    # make the top.gg api client
+    topggclient = dbl.DBLClient(bot, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQzNzQzOTU2MjM4NjUwNTczMCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTg1ODA5MDQ2fQ.5IZ449Tf5mj5ZEaXORVKuZ2SKL6KcaySkgE8unc59-4")
+
+
 # remove the help so we can have a custom one
 bot.remove_command('help')
+
+@commands.command(name='help')
+async def newhelp(ctx):
+    # custom help command
+    # make the embeds
+    helpembed = discord.Embed(title="**http://rbdis.xyz**",
+                         description="help",
+                         color=red)
+    if production:
+        botinfo = await topggclient.get_bot_info()
+        getto = botinfo.get('monthlyPoints') + 10
+        helpembed.add_field(name="\n\nThe bot currently has **" + str(botinfo.get('monthlyPoints')) + "** votes, can we get it to **" + str(getto) + "**?", value='https://top.gg/bot/437439562386505730/vote', inline=False)
+    helpembed.add_field(name="Join the server!", value="http://rbdis.xyz/server/", inline=False)
+    helpembed.add_field(name="Found a bug?", value="Report it here http://rbdis.xyz/bugreport/", inline=False)
+    helpembed.add_field(name="Please give your feedback!", value="http://rbdis.xyz/feedback", inline=False)
+
+    commandsembed = discord.Embed(title="Help:",
+                         description="**Commands**",
+                         color=red)
+    commandsembed.add_field(name="rhelp", value="Shows this page", inline=False)
+    commandsembed.add_field(name="rr [sub name here]", value="Gives you some info on a subreddit", inline=False)
+    commandsembed.add_field(name="ru [username here]", value="Gives you some info on a user", inline=False)
+    commandsembed.add_field(name="rforce [sub name here]", value="Removes subreddit cache", inline=False)
+    await ctx.author.send(embed=helpembed)
+    await ctx.author.send(embed=commandsembed)
+# add the new help command
+bot.add_command(newhelp)
+
+# update status
+@commands.command()
+async def update(ctx):
+    await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(name="rhelp | In " + str(len(bot.guilds)) + " servers"))
+bot.add_command(update)
 
 # all the cogs
 extensions = ["user", "subreddit", "utils"]
