bdf83a829bea2bd3262f36582a8c27f8880282de
bwac <40532058+BWACpro@users.noreply.github.com>
2020-04-12 18:33:32 +1000

    Fixed more :p

--- versions/v1.3/1.3/1.3.1/topgg.py
+++ versions/v1.3/1.3/1.3.1/topgg.py
@@ -11,7 +11,7 @@ class topgg(commands.Cog):
         self.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQzNzQzOTU2MjM4NjUwNTczMCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTg1ODA5MDQ2fQ.5IZ449Tf5mj5ZEaXORVKuZ2SKL6KcaySkgE8unc59-4"
         self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True)
 
-    async def on_guild_post(self, ctx):
+    async def on_guild_post():
         await bot.change_presence(status=discord.Status.do_not_disturb,
                                   activity=discord.Game(name="rhelp | In " + str(len(ctx.guilds)) + " servers"))
         print("Server count posted successfully")
