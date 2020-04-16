0fcd2a13759c386202825dbc57bf834f205e4fda
bwac <40532058+BWACpro@users.noreply.github.com>
2020-04-16 19:46:34 +1000

    on_command_error description

--- bot/bot.py
+++ bot/bot.py
@@ -72,6 +72,7 @@ bot.add_command(update)
 
 @bot.event
 async def on_command_error(ctx, error):
+    """Sends error to user and channel"""
     await ctx.send(str(ctx.author)+', something went wrong. \n`'+str(error)+'`\nIf it keeps happening report it here https://rbdis.xyz/bugreport or https://rbdis.xyz/server\nThank you!')
 
 # all the cogs
