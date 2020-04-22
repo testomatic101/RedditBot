1ab8cf6155eb83d05d7b00ae37424b6c4a54f2f9
bwac <40532058+BWACpro@users.noreply.github.com>
2020-04-22 20:53:04 +1000

    improved r!subreddit

--- bot/cogs/subreddit.py
+++ bot/cogs/subreddit.py
@@ -27,167 +27,129 @@ class subreddit(commands.Cog):
         """Subreddit command, r!subreddit, s, sub"""
 
         if subreddit_name:
-            if ctx.guild:
-                loading = discord.Embed(title="", color=red)
-                loading.add_field(
-                    name="Loading...",
-                    value="<a:loading:650579775433474088> Contacting reddit "
-                    "servers...",
-                )
-                loading.set_footer(
-                    text="if it never loads, RedditBot can't find the subreddit"
-                )
-                loadingMessage = await ctx.send(embed=loading)
-
+            if ctx.channel.is_nsfw():
                 reddit = praw.Reddit(
                     client_id=secrets["reddit_id"],
                     client_secret=secrets["reddit_secret"],
                     user_agent="discord:n/a:" + version_number + " (by /u/-_-BWAC-_-)",
                 )
-
                 subreddit = reddit.subreddit(subreddit_name)
-                if ctx.channel.is_nsfw():
+
+                loading = discord.Embed(title="", color=red)
+                loading.add_field(
+                    name="Cache...",
+                    value="<a:loading:650579775433474088> checking if this has been cached",
+                )
+                loading.set_footer(
+                    text="if it never loads, something went wrong in the backround, or the subreddit cant be found"
+                )
+                loadingMessage = await ctx.send(embed=loading)
+                time_cached = None
+                smalldes = None
+                subcount = None
+                nsfw = None
+                thumbnail = None
+                if os.path.isfile("cache/subreddits/" + subreddit_name + ".json"):
+                    # If cache exists, read from it
                     loading = discord.Embed(title="", color=red)
                     loading.add_field(
                         name="Cache...",
-                        value="<a:loading:650579775433474088> checking if this has been cached",
+                        value="<a:loading:650579775433474088> cache found! now loading from",
                     )
                     loading.set_footer(
                         text="if it never loads, something went wrong in the backround, or the subreddit cant be found"
                     )
                     await loadingMessage.edit(embed=loading)
-
-                    time_cached = None
-                    smalldes = None
-                    subcount = None
-                    nsfw = None
-                    thumbnail = None
-
-                    if os.path.isfile("cache/subreddits/" + subreddit_name + ".json"):
-                        # If cache exists, read from it
-                        loading = discord.Embed(title="", color=red)
-                        loading.add_field(
-                            name="Cache...",
-                            value="<a:loading:650579775433474088> cache found! now loading from",
-                        )
-                        loading.set_footer(
-                            text="if it never loads, something went wrong in the backround, or the subreddit cant be found"
-                        )
-                        await loadingMessage.edit(embed=loading)
-
-                        with open(
-                            "cache/subreddits/" + subreddit_name + ".json"
-                        ) as json_file:
-                            cache = json.load(json_file)
-
-                            time_cached = cache["time_cached"]
-                            smalldes = cache["smalldes"]
-                            subcount = cache["subcount"]
-                            nsfw = cache["nsfw"]
-                            thumbnail = cache["thumbnail"]
-                    else:
-                        # If cache doesnt exit, make it
-                        loading = discord.Embed(title="", color=red)
-                        loading.add_field(
-                            name="Cache...",
-                            value="<a:loading:650579775433474088> cache not found.. creating",
-                        )
-                        loading.set_footer(
-                            text="if it never loads, something went wrong in the backround, or the subreddit cant be found"
-                        )
-                        await loadingMessage.edit(embed=loading)
-
-                        try:
-                            smalldes = subreddit.public_description
-                        except:
-                            smalldes = None
-                        subcount = subreddit.subscribers
-                        nsfw = subreddit.over18
-                        thumbnail = subreddit.icon_img
-
-                        cache = {
-                            "time_cached": str(datetime.datetime.now()),
-                            "smalldes": smalldes,
-                            "subcount": subcount,
-                            "nsfw": nsfw,
-                            "thumbnail": thumbnail,
-                        }
-                        with open(
-                            "cache/subreddits/" + subreddit_name + ".json", "w"
-                        ) as outfile:
-                            json.dump(cache, outfile)
-
-                    datetime.datetime.fromtimestamp(
-                        int(subreddit.created_utc)
-                    ).strftime("%m/%d/%Y")
-                    sub = discord.Embed(
-                        title="r/" + subreddit_name + " info:", color=red
+                    with open(
+                        "cache/subreddits/" + subreddit_name + ".json"
+                    ) as json_file:
+                        cache = json.load(json_file)
+                        time_cached = cache["time_cached"]
+                        smalldes = cache["smalldes"]
+                        subcount = cache["subcount"]
+                        nsfw = cache["nsfw"]
+                        thumbnail = cache["thumbnail"]
+                else:
+                    # If cache doesnt exit, make it
+                    loading = discord.Embed(title="", color=red)
+                    loading.add_field(
+                        name="Cache...",
+                        value="<a:loading:650579775433474088> cache not found.. creating",
                     )
-                    if smalldes:
-                        sub.add_field(
-                            name="\nSmall Description:", value=smalldes, inline=False
-                        )
-                    else:
-                        sub.add_field(
-                            name="\There is no small description for this subreddit",
-                            value="*nothing*",
-                            inline=False,
-                        )
-                    sub.add_field(name="\nSubscriber Count:", value=subcount)
-                    sub.add_field(name="NSFW:", value=nsfw)
-                    if time_cached:
-                        sub.add_field(
-                            name="*these results are from a cache made at*:",
-                            value=time_cached,
-                            inline=False,
-                        )
-                        sub.add_field(
-                            name="*if you want the latest stats, use r!resetsub "
-                            + subreddit_name
-                            + "*",
-                            value="keep in mind that you should only reset a subreddit cache every so often",
-                            inline=False,
-                        )
-
+                    loading.set_footer(
+                        text="if it never loads, something went wrong in the backround, or the subreddit cant be found"
+                    )
+                    await loadingMessage.edit(embed=loading)
+                    try:
+                        smalldes = subreddit.public_description
+                    except:
+                        smalldes = None
+                    subcount = subreddit.subscribers
+                    nsfw = subreddit.over18
+                    thumbnail = subreddit.icon_img
+                    cache = {
+                        "time_cached": str(datetime.datetime.now()),
+                        "smalldes": smalldes,
+                        "subcount": subcount,
+                        "nsfw": nsfw,
+                        "thumbnail": thumbnail,
+                    }
+                    with open(
+                        "cache/subreddits/" + subreddit_name + ".json", "w"
+                    ) as outfile:
+                        json.dump(cache, outfile)
+                datetime.datetime.fromtimestamp(int(subreddit.created_utc)).strftime(
+                    "%m/%d/%Y"
+                )
+                sub = discord.Embed(title="r/" + subreddit_name + " info:", color=red)
+                if smalldes:
                     sub.add_field(
-                        name="maybe try:",
-                        value="try r!hot "
-                        + subreddit_name
-                        + " and r!top "
-                        + subreddit_name
-                        + " to show the top 10 top and hot posts",
+                        name="\nSmall Description:", value=smalldes, inline=False
+                    )
+                else:
+                    sub.add_field(
+                        name="\There is no small description for this subreddit",
+                        value="*nothing*",
                         inline=False,
                     )
-
-                    sub.set_author(
-                        name="RedditBot",
-                        icon_url="https://images.discordapp.net/avatars/437439562386505730/2874f76dd780cb0af624e3049a6bfad0.png",
+                sub.add_field(name="\nSubscriber Count:", value=subcount)
+                sub.add_field(name="NSFW:", value=nsfw)
+                if time_cached:
+                    sub.add_field(
+                        name="*these results are from a cache made at*:",
+                        value=time_cached,
+                        inline=False,
                     )
-                    sub.set_thumbnail(url=thumbnail)
-                    sub.set_footer(text="RedditBot " + version)
-                    await loadingMessage.edit(embed=sub)
-                else:
-                    error = discord.Embed(title="Error", color=red)
-                    error.add_field(
-                        name="Sorry",
-                        value="The channel **"
-                        + ctx.channel.name
-                        + "** is not nsfw, to be safe "
-                        "with the discord tos and "
-                        "such, you will have to "
-                        "change the channel to nsfw.",
+                    sub.add_field(
+                        name="*if you want the latest stats, use r!resetsub "
+                        + subreddit_name
+                        + "*",
+                        value="keep in mind that you should only reset a subreddit cache every so often",
+                        inline=False,
                     )
-                    await loadingMessage.edit(embed=error)
+                sub.set_author(
+                    name="RedditBot",
+                    icon_url="https://images.discordapp.net/avatars/437439562386505730/2874f76dd780cb0af624e3049a6bfad0.png",
+                )
+                sub.set_thumbnail(url=thumbnail)
+                sub.set_footer(text="RedditBot " + version)
+                await loadingMessage.edit(embed=sub)
             else:
-                await ctx.send("Please do this in a server")
+                errorEmbed = discord.Embed(
+                    title=str(ctx.author) + " Use this in a nsfw channel",
+                    description="This is to be safe with discord tos",
+                    color=red,
+                )
+                errorEmbed.set_footer(text=version_number)
+                await ctx.send(embed=errorEmbed)
         else:
-            error = discord.Embed(
-                title="You didn't give a subreddit!\n\nYou should use this command like:\nr!subreddit `"
-                "subreddit name`",
+            errorEmbed = discord.Embed(
+                title=str(ctx.author) + " You didn't give a subreddit",
+                description="You should use this command like:\nr!subreddit `subreddit name`",
                 color=red,
             )
-            error.set_footer(text=version)
-            await ctx.send(embed=error)
+            errorEmbed.set_footer(text=version_number)
+            await ctx.send(embed=errorEmbed)
 
     @commands.command(aliases=["t"])
     async def top(self, ctx, subreddit_name=None):
